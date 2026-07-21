c3b96c1c9ce305b514c1751ae991a6fcf782cde8
shreyasync <shreyapandey1465@gmail.com>
2026-07-21 23:20:19 +0530

    Add factorial calculation program in C

---
+++ Day3/factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i , n , fact = 1;
+    printf ("Enter number :");
+    scanf("%d" , &n);
+    
+    for( i = 1; i <= n; i++){
+        fact *= i;
+    }
+    printf("factorial %d =%d" , n ,fact);
+    return 0;
+}
