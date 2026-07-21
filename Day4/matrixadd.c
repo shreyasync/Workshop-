05c1e61e8bc2dcfd60ab0dca031891bb7f98f359
shreyasync <shreyapandey1465@gmail.com>
2026-07-21 23:26:26 +0530

    Implement matrix addition in C

---
+++ Day4/matrixadd.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+
+    int m, n;
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+    int a[m][n], b[m][n], c[m][n];
+    printf("Enter the elements of the first matrix:\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter the elements of the second matrix:\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    // add matrices
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("The sum of the matrices is:\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
