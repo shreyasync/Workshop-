d3304eb374045b7b70e4a4a5358cc3c144fa427d
shreyasync <shreyapandey1465@gmail.com>
2026-07-21 15:48:12 +0530

    Add area calculation program for different shapes

---
+++ Day1/area.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main ()
+{
+    int choice;
+    float side,length,breadth,area,radius;
+    
+    printf("Enter the shape\n");
+    printf("1. Square\n");
+    printf("2.Rectangle\n");
+    printf("3.Circle\n");
+    
+    printf("Enter your choice:");
+    scanf("%d", &choice);
+    
+    switch(choice)
+    {
+        case 1:
+        printf("Enter side:");
+        scanf("%f",&side);
+        area = side * side;
+        printf("Area of square = %.2f",area);
+        break;
+        
+        case 2:
+        printf("Enter length and breadth:");
+        scanf("%f %f", &length, &breadth);
+        area = length * breadth;
+        printf("Area of rectangle = %.2f",area);
+        break;
+        
+        case 3:
+        printf("Enter the radius:");
+        scanf("%f", &radius);
+        area = 3.14*radius*radius;
+        printf("Area of circle = %.2f",area);
+        break;
+        
+        default:
+        printf("Invalid Choice");
+    }
+    
+    return 0;
+}
